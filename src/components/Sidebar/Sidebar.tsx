18e003e9e012ffb23b2019a7690bd3370b2f3afb
Denis Sitko <Denis.Sitko@aspose.com>
2025-01-12 17:29:05 +0200

    Add Sidebar Links

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,36 +1,49 @@
+import { link } from "fs";
 import "./Sidebar.css";
+import { useNavigate } from "react-router-dom";
 
 const sidebarItems = [
   {
     icon: "",
     name: "Counter",
+    linkTo: "/Users",
   },
   {
     icon: "",
     name: "Claims",
+    linkTo: "/Users",
   },
   {
     icon: "",
     name: "Roles",
+    linkTo: "/Users",
   },
   {
     icon: "",
     name: "Users",
+    linkTo: "/Users",
   },
   {
     icon: "",
     name: "Access Control",
+    linkTo: "/AccessControl",
   },
 ];
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <aside className="layout_sidebar">
       <div className="layout_sidebar-header">Flexible Auth</div>
       <ul className="layout_sidebar-list">
-        {sidebarItems.map(({ name }) => {
+        {sidebarItems.map(({ name, linkTo }) => {
           return (
-            <li key={name} className="layout_sidebar-list-item">
+            <li
+              key={name}
+              className="layout_sidebar-list-item"
+              onClick={() => navigate(linkTo)}
+            >
               {name}
             </li>
           );
